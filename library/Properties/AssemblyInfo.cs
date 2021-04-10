b6c05d1be5f33f25970a81c7b127e4cf3f17b0cb
Germán Berná Martínez <gbm25@gcloud.ua.es>
2021-04-10 19:13:03 +0200

    EN/CAD usuario

--- library/Properties/AssemblyInfo.cs
+++ library/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("c1943eee-f560-4929-b9fa-6c28b12d6c2d")]
+[assembly: Guid("b9cf40a9-caea-4b1f-9070-71e9c1829f5b")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
